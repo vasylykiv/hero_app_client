f81116a0020e2ccf14184e49d9bf974378d2f0a7
Vitaliy <vasylykivit@gmail.com>
2025-09-15 11:37:50 +0300

    000(Init): init project

---
+++ rsbuild.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
+
+export default defineConfig({
+  plugins: [pluginReact(), pluginSvgr()],
+  html: {
+    template: "./public/index.html",
+  },
+});
